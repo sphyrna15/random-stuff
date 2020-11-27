626b70a823b2f4f84cd27e26512f782428c88b01
sphyrna15 <timmysnakes@gmail.com>
2020-11-27 16:47:00 +0100

    add more data

--- 80_Pendulum-analysis.py
+++ 80_Pendulum-analysis.py
@@ -14,15 +14,17 @@ import matplotlib.pyplot as plt
 
 # Read CSV files
 
-raw665 = np.genfromtxt('Data/Pendulum-Data/length665-1/Raw Data.csv',delimiter=',')[1:]
+raw665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Raw Data.csv',delimiter=',')[1:]
+auto665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Autocorrelation.csv',delimiter=',')[1:]
+reso665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Resonance.csv',delimiter=',')[1:]
 
-auto665 = np.genfromtxt('Data/Pendulum-Data/length665-1/Autocorrelation.csv',delimiter=',')[1:]
+raw665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Raw Data.csv',delimiter=',')[1:]
+auto665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Autocorrelation.csv',delimiter=',')[1:]
+reso665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Resonance.csv',delimiter=',')[1:]
 
-reso665 = np.genfromtxt('Data/Pendulum-Data/length665-1/Resonance.csv',delimiter=',')[1:]
-
-
-
-
+raw665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Raw Data.csv',delimiter=',')[1:]
+auto665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Autocorrelation.csv',delimiter=',')[1:]
+reso665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Resonance.csv',delimiter=',')[1:]
 
 
 
@@ -30,26 +32,58 @@ reso665 = np.genfromtxt('Data/Pendulum-Data/length665-1/Resonance.csv',delimiter
 # PLOT DATA
 
 
-f1, a1 = plt.subplots(2, 1, sharex=True, figsize=(6,6))
+# RAW DATA 
 
-a1[0].plot(auto665[:,0], auto665[:,1], 'k-', label='Autocorr')
+fp, a1 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+a1[0].plot(raw665_1[:,0], raw665_1[:,1], 'b-', label='x')
 a1[0].legend()
 a1[0].grid()
 
-a1[1].plot(reso665[:,0], reso665[:,1], 'y-', label='Resonance')
+a1[1].plot(raw665_1[:,0], raw665_1[:,2], 'r-', label='y')
 a1[1].legend()
 a1[1].grid()
 
-fp, ap = plt.subplots(3, 1, sharex=True, figsize=(6,6))
-ap[0].plot(raw665[:,0], raw665[:,1], 'b-', label='x')
-ap[0].legend()
-ap[0].grid()
+a1[2].plot(raw665_1[:,0], raw665_1[:,3], 'g-', label='z')
+a1[2].legend()
+a1[2].grid()
 
-ap[1].plot(raw665[:,0], raw665[:,2], 'r-', label='y')
-ap[1].legend()
-ap[1].grid()
+fp, a2 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+a2[0].plot(raw665_2[:,0], raw665_2[:,1], 'b-', label='x')
+a2[0].legend()
+a2[0].grid()
 
-ap[2].plot(raw665[:,0], raw665[:,3], 'g-', label='z')
-ap[2].legend()
-ap[2].grid()
+a2[1].plot(raw665_2[:,0], raw665_2[:,2], 'r-', label='y')
+a2[1].legend()
+a2[1].grid()
+
+a2[2].plot(raw665_2[:,0], raw665_2[:,3], 'g-', label='z')
+a2[2].legend()
+a2[2].grid()
+
+fp, a3 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+a3[0].plot(raw665_3[:,0], raw665_3[:,1], 'b-', label='x')
+a3[0].legend()
+a3[0].grid()
+
+a3[1].plot(raw665_3[:,0], raw665_3[:,2], 'r-', label='y')
+a3[1].legend()
+a3[1].grid()
+
+a3[2].plot(raw665_3[:,0], raw665_3[:,3], 'g-', label='z')
+a3[2].legend()
+a3[2].grid()
+
+
+
+# # Autocorrelation and Resonance 
+
+# f1, ar = plt.subplots(2, 1, sharex=True, figsize=(6,6))
+
+# ar[0].plot(auto665_1[:,0], auto665_1[:,1], 'k-', label='Autocorr')
+# ar[0].legend()
+# ar[0].grid()
+
+# ar[1].plot(reso665_1[:,0], reso665_1[:,1], 'y-', label='Resonance')
+# ar[1].legend()
+# ar[1].grid()
 
