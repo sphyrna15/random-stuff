309d852d3cf6df6457a597a6685c899862de1389
sphyrna15 <timmysnakes@gmail.com>
2020-12-01 21:59:03 +0100

    Decay Coeff. calculation functions finished - no bugs

--- 80_Pendulum-analysis.py
+++ 80_Pendulum-analysis.py
@@ -16,18 +16,22 @@ from scipy.optimize import curve_fit
 ########### Read CSV files
 
 raw665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Raw Data.csv',delimiter=',')[1:]
-auto665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Autocorrelation.csv',delimiter=',')[1:]
-reso665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Resonance.csv',delimiter=',')[1:]
-
+# auto665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Autocorrelation.csv',delimiter=',')[1:]
+# reso665_1 = np.genfromtxt('Data/Pendulum-Data/length665-1/Resonance.csv',delimiter=',')[1:]
 raw665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Raw Data.csv',delimiter=',')[1:]
-auto665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Autocorrelation.csv',delimiter=',')[1:]
-reso665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Resonance.csv',delimiter=',')[1:]
-
+# auto665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Autocorrelation.csv',delimiter=',')[1:]
+# reso665_2 = np.genfromtxt('Data/Pendulum-Data/length665-2/Resonance.csv',delimiter=',')[1:]
 raw665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Raw Data.csv',delimiter=',')[1:]
-auto665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Autocorrelation.csv',delimiter=',')[1:]
-reso665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Resonance.csv',delimiter=',')[1:]
+# auto665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Autocorrelation.csv',delimiter=',')[1:]
+# reso665_3 = np.genfromtxt('Data/Pendulum-Data/length665-3/Resonance.csv',delimiter=',')[1:]
 
+raw690_1 = np.genfromtxt('Data/Pendulum-Data/length690-1/Raw Data.csv',delimiter=',')[1:]
+raw690_2 = np.genfromtxt('Data/Pendulum-Data/length690-2/Raw Data.csv',delimiter=',')[1:]
+raw690_3 = np.genfromtxt('Data/Pendulum-Data/length690-3/Raw Data.csv',delimiter=',')[1:]
 
+raw760_1 = np.genfromtxt('Data/Pendulum-Data/length760-1/Raw Data.csv',delimiter=',')[1:]
+raw760_2 = np.genfromtxt('Data/Pendulum-Data/length760-2/Raw Data.csv',delimiter=',')[1:]
+raw760_3 = np.genfromtxt('Data/Pendulum-Data/length760-3/Raw Data.csv',delimiter=',')[1:]
 
 ############################ DATA ANALYSIS ####################################
 
@@ -94,7 +98,7 @@ def get_times(time_data, indices):
 
 
 # Function to fully calculate the decay coeff. gamma and plot the decay
-def calculate_gamma(data, time_data, tol, plot = True):
+def calculate_gamma(data, time_data, tol, plot = True, title = 'Title'):
     
     extrema, indices = find_turns(data, tol)
     time = get_times(time_data, indices) # Not sure yet how to structure time
@@ -112,7 +116,7 @@ def calculate_gamma(data, time_data, tol, plot = True):
     plt.figure()
     plt.plot(t, extrema, 'b-', label='Extrema')
     plt.plot(t, decay(t, gamma_value[0]), 'k--', label='Fitted Decay Function')
-    plt.title('Decay of Turning Points of the Oscillation')
+    plt.title(title)
     plt.xlabel('Time $t$ (s)')
     plt.ylabel('Amplitude')
     plt.legend()
@@ -120,59 +124,143 @@ def calculate_gamma(data, time_data, tol, plot = True):
     plt.show()
     
     return gamma_value[0], extrema, indices, time
-    
-gamma665, ex665, idx665, t665 = calculate_gamma(raw665_3[:,3], raw665_3[:,0], 10)
 
-print()
-print('Value for Gamma is: ' + str(gamma665))
-print()
 
-# PLOT DATA
+########### Calculate Gammas 
+
+gamma665_1, ex765_1, idx665_1, t665_1 = calculate_gamma(raw665_1[:,1], raw665_1[:,0], 10, 
+                                                        title = 'L-665 1', plot=False)
+
+gamma665_2, ex765_2, idx665_2, t665_2 = calculate_gamma(raw665_2[:,1], raw665_2[:,0], 10, 
+                                                        title = 'L-665 2', plot=False)
+
+gamma665_3, ex665_3, idx665_3, t665_3 = calculate_gamma(raw665_3[:,3], raw665_3[:,0], 10,
+                                                        title = 'L-690 3', plot=False)
+
+gamma760_1, ex760_1, idx760_1, t760_1 = calculate_gamma(raw760_1[:,1], raw760_1[:,0], 10, 
+                                                        title = 'L-760 1', plot=False)
+
+gamma760_2, ex760_2, idx760_2, t760_2 = calculate_gamma(raw760_2[:,1], raw760_2[:,0], 10, 
+                                                        title = 'L-760 2', plot=False)
+
+gamma760_3, ex760_3, idx760_3, t760_3 = calculate_gamma(raw760_3[:,1], raw760_3[:,0], 10, 
+                                                        title = 'L-760 3', plot=False)
+
+gamma690_1, ex760_1, idx690_1, t690_1 = calculate_gamma(raw690_1[:,1], raw690_1[:,0], 10, 
+                                                        title = 'L-690 1', plot=False)
+
+gamma690_2, ex760_2, idx690_2, t690_2 = calculate_gamma(raw690_2[:,1], raw690_2[:,0], 10, 
+                                                        title = 'L-690 2', plot=False)
+
+gamma690_3, ex760_3, idx690_3, t690_3 = calculate_gamma(raw690_3[:,1], raw690_3[:,0], 10, 
+                                                        title = 'L-690 3', plot=False)
+
+# Print Decay coefficients
+print()
+print('Gamma for L-665 1: ' + str(gamma665_1))
+print('Gamma for L-665 2: ' + str(gamma665_2))
+print('Gamma for L-665 3: ' + str(gamma665_3))
+print()
+print('Gamma for L-760 1: ' + str(gamma760_1))
+print('Gamma for L-760 2: ' + str(gamma760_2))
+print('Gamma for L-760 3: ' + str(gamma760_3))
+print()
+print('Gamma for L-690 1: ' + str(gamma690_1))
+print('Gamma for L-690 2: ' + str(gamma690_2))
+print('Gamma for L-690 3: ' + str(gamma690_3))
+print()
 
 
 ################ RAW DATA PLOTS #############################
 
+############## Length 760 1 and 2
+
 # fp, a1 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
-# a1[0].plot(raw665_1[:,0], raw665_1[:,1], 'b-', label='x')
+# a1[0].plot(raw760_1[:,0], raw760_1[:,1], 'b-', label='x')
+# a1[0].plot(t760_1, get_times(raw760_1[:,1], idx760_1), 'bo', label='extrema')
 # a1[0].legend()
 # a1[0].grid()
 
-# a1[1].plot(raw665_1[:,0], raw665_1[:,2], 'r-', label='y')
+# a1[1].plot(raw760_1[:,0], raw760_1[:,2], 'r-', label='y')
 # a1[1].legend()
 # a1[1].grid()
 
-# a1[2].plot(raw665_1[:,0], raw665_1[:,3], 'g-', label='z')
+# a1[2].plot(raw760_1[:,0], raw760_1[:,3], 'g-', label='z')
 # a1[2].legend()
 # a1[2].grid()
 
 # fp, a2 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
-# a2[0].plot(raw665_2[:,0], raw665_2[:,1], 'b-', label='x')
+# a2[0].plot(raw760_2[:,0], raw760_2[:,1], 'b-', label='x')
+# a2[0].plot(t760_2, get_times(raw760_2[:,1], idx760_2), 'bo', label='extrema')
 # a2[0].legend()
 # a2[0].grid()
 
-# a2[1].plot(raw665_2[:,0], raw665_2[:,2], 'r-', label='y')
+# a2[1].plot(raw760_2[:,0], raw760_2[:,2], 'r-', label='y')
 # a2[1].legend()
 # a2[1].grid()
 
-# a2[2].plot(raw665_2[:,0], raw665_2[:,3], 'g-', label='z')
+# a2[2].plot(raw760_2[:,0], raw760_2[:,3], 'g-', label='z')
 # a2[2].legend()
 # a2[2].grid()
 
-fp, a3 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
-a3[0].plot(raw665_3[:,0], raw665_3[:,1], 'b-', label='x')
-a3[0].legend()
-a3[0].grid()
 
-a3[1].plot(raw665_3[:,0], raw665_3[:,2], 'r-', label='y')
-a3[1].legend()
-a3[1].grid()
+########### Length 665 1
 
-a3[2].plot(raw665_3[:,0], raw665_3[:,3], 'g-', label='z')
-# a3[2].plot(t[0], res[0], 'bo', label='origin point')
-# a3[2].plot(t, f(t, *params), 'k--', label='fit')
-plt.plot(t665, get_times(raw665_3[:,3], idx665), 'bo', label='extrema')
-a3[2].legend()
-a3[2].grid()
+# fp, a3 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+# a3[0].plot(raw665_3[:,0], raw665_3[:,1], 'b-', label='x')
+# a3[0].legend()
+# a3[0].grid()
+
+# a3[1].plot(raw665_3[:,0], raw665_3[:,2], 'r-', label='y')
+# a3[1].legend()
+# a3[1].grid()
+
+# a3[2].plot(raw665_3[:,0], raw665_3[:,3], 'g-', label='z')
+# plt.plot(t665, get_times(raw665_3[:,3], idx665), 'bo', label='extrema')
+# a3[2].legend()
+# a3[2].grid()
+
+
+########### Length 690 1, 2 and 3
+
+# fp, a4 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+# a4[0].plot(raw690_3[:,0], raw690_3[:,1], 'b-', label='x')
+# a4[0].legend()
+# a4[0].grid()
+
+# a4[1].plot(raw690_3[:,0], raw690_3[:,2], 'r-', label='y')
+# a4[1].legend()
+# a4[1].grid()
+
+# a4[2].plot(raw690_3[:,0], raw690_3[:,3], 'g-', label='z')
+# a4[2].legend()
+# a4[2].grid()
+
+# fp, a5 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+# a5[0].plot(raw690_2[:,0], raw690_2[:,1], 'b-', label='x')
+# a5[0].legend()
+# a5[0].grid()
+
+# a5[1].plot(raw690_2[:,0], raw690_2[:,2], 'r-', label='y')
+# a5[1].legend()
+# a5[1].grid()
+
+# a5[2].plot(raw690_2[:,0], raw690_2[:,3], 'g-', label='z')
+# a5[2].legend()
+# a5[2].grid()
+
+# fp, a6 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
+# a6[0].plot(raw690_1[:,0], raw690_1[:,1], 'b-', label='x')
+# a6[0].legend()
+# a6[0].grid()
+
+# a6[1].plot(raw690_1[:,0], raw690_1[:,2], 'r-', label='y')
+# a6[1].legend()
+# a6[1].grid()
+
+# a6[2].plot(raw690_1[:,0], raw690_1[:,3], 'g-', label='z')
+# a6[2].legend()
+# a6[2].grid()
 
 
 ############## Autocorrelation and Resonance ###################
