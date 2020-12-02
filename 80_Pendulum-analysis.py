4222324101926905034017b0aa5eab92cb62040e
sphyrna15 <timmysnakes@gmail.com>
2020-12-02 21:28:54 +0100

    Add calculations for g, plus plots

--- 80_Pendulum-analysis.py
+++ 80_Pendulum-analysis.py
@@ -129,33 +129,33 @@ def calculate_gamma(data, time_data, tol, plot = True, title = 'Title'):
 
 ##### Length 665 mm
 gamma665_1, ex665_1, idx665_1, t665_1 = calculate_gamma(raw665_1[:,1], raw665_1[:,0], 10, 
-                                                        title = 'L-665 1', plot=False)
+                                                        title = 'L-665 1', plot = False)
 
 gamma665_2, ex665_2, idx665_2, t665_2 = calculate_gamma(raw665_2[:,1], raw665_2[:,0], 10, 
-                                                        title = 'L-665 2', plot=False)
+                                                        title = 'L-665 2', plot = False)
 
 gamma665_3, ex665_3, idx665_3, t665_3 = calculate_gamma(raw665_3[:,3], raw665_3[:,0], 10,
-                                                        title = 'L-690 3', plot=False)
+                                                        title = 'L-690 3', plot = False)
 
 ##### Length 760 mm
-gamma760_1, ex760_1, idx760_1, t760_1 = calculate_gamma(raw760_1[:,1], raw760_1[:,0], 10, 
-                                                        title = 'L-760 1', plot=False)
+gamma760_1, ex760_1, idx760_1, t760_1 = calculate_gamma(raw760_1[:,1], raw760_1[:,0], 5, 
+                                                        title = 'L-760 1', plot = False)
 
 gamma760_2, ex760_2, idx760_2, t760_2 = calculate_gamma(raw760_2[:,1], raw760_2[:,0], 10, 
-                                                        title = 'L-760 2', plot=False)
+                                                        title = 'L-760 2', plot = False)
 
 gamma760_3, ex760_3, idx760_3, t760_3 = calculate_gamma(raw760_3[:,1], raw760_3[:,0], 10, 
-                                                        title = 'L-760 3', plot=False)
+                                                        title = 'L-760 3', plot = False)
 
 ##### Length 690 mm
 gamma690_1, ex690_1, idx690_1, t690_1 = calculate_gamma(raw690_1[:,1], raw690_1[:,0], 10, 
-                                                        title = 'L-690 1', plot=False)
+                                                        title = 'L-690 1', plot = False)
 
 gamma690_2, ex690_2, idx690_2, t690_2 = calculate_gamma(raw690_2[:,1], raw690_2[:,0], 10, 
-                                                        title = 'L-690 2', plot=False)
+                                                        title = 'L-690 2', plot = False)
 
 gamma690_3, ex690_3, idx690_3, t690_3 = calculate_gamma(raw690_3[:,1], raw690_3[:,0], 10, 
-                                                        title = 'L-690 3', plot=False)
+                                                        title = 'L-690 3', plot = False)
 
 # Print Decay coefficients
 print()
@@ -172,14 +172,35 @@ print('Gamma for L-690 2: ' + str(gamma690_2))
 print('Gamma for L-690 3: ' + str(gamma690_3))
 print()
 
+gammas = np.array([0.179, 0.120, 0.176, 0.148, 0.130, 0.161, 0.174]) #only useful ones
+gamma_avg = np.average(gammas)
+gamma_std = np.std(gammas)
+
+print('Average gamma: ' + str(gamma_avg))
+print('With standard deviation of: ' + str(gamma_std))
+print()
+
 
 ################ RAW DATA PLOTS #############################
+
+
+##### best example for report
+
+# plt.figure()
+# plt.plot(raw760_1[:,0], raw760_1[:,1], 'b-', label='x')
+# plt.plot(t760_1, get_times(raw760_1[:,1], idx760_1), 'ro', label='extrema')
+# plt.title('Dataset 1 Length 760 mm')
+# plt.ylabel('Amplitude')
+# plt.xlabel('Time in s')
+# plt.legend()
+# plt.grid()
+# plt.show()
 
 ############## Length 760 1 and 2
 
 # fp, a1 = plt.subplots(3, 1, sharex=True, figsize=(6,6))
 # a1[0].plot(raw760_1[:,0], raw760_1[:,1], 'b-', label='x')
-# a1[0].plot(t760_1, get_times(raw760_1[:,1], idx760_1), 'bo', label='extrema')
+# a1[0].plot(t760_1, get_times(raw760_1[:,1], idx760_1), 'ro', label='extrema')
 # a1[0].legend()
 # a1[0].grid()
 
@@ -218,7 +239,7 @@ print()
 # a3[1].grid()
 
 # a3[2].plot(raw665_3[:,0], raw665_3[:,3], 'g-', label='z')
-# plt.plot(t665, get_times(raw665_3[:,3], idx665), 'bo', label='extrema')
+# a3[2].plot(t665_3, get_times(raw665_3[:,3], idx665_3), 'bo', label='extrema')
 # a3[2].legend()
 # a3[2].grid()
 
@@ -276,5 +297,64 @@ print()
 # ar[1].plot(reso665_1[:,0], reso665_1[:,1], 'y-', label='Resonance')
 # ar[1].legend()
 # ar[1].grid()
+
+
+
+
+########################## PART 1 GRAVIATIONAL CONSTANT ############################
+
+#### Tim data
+ltim = np.array([75.0, 75.0, 69.50, 69.50, 69.50, 66.50, 66.50, 66.50])
+Ttim = np.array([1.73, 1.72, 1.66, 1.68, 1.67, 1.63, 1.63, 1.64])
+ftim = np.array([0.58, 0.58, 0.60, 0.60, 0.60, 0.60, 0.61, 0.61])
+gtim = np.array([9.85, 9.99, 9.92, 9.77, 9.82, 9.85, 9.85, 9.75])
+# gtim = 4 * ltim * np.pi**2 / (Ttim**2 * 100)
+gt_avg = np.average(gtim)
+gt_std = np.std(gtim)
+
+
+#### Mario data
+lmar = np.array([60.5, 56.00, 60.0,59.1, 49.9, 42.1, 44.8])
+Tmar = np.array([1.56, 1.54, 1.56, 1.55, 1.44, 1.33, 1.38])
+fmar = None
+gmar = np.array([9.79, 9.32, 9.71, 9.70, 9.54, 9.39, 9.31])
+gmar_avg = np.average(gmar)
+gmar_std = np.std(gmar)
+
+
+#### Full Dataset
+l = np.concatenate((ltim, lmar))
+T = np.concatenate((Ttim, Tmar))
+g = np.concatenate((gtim, gmar))
+
+gavg = np.average(g)
+gstd = np.std(g)
+
+print()
+print('Average g is: ' + str(gavg))
+print('With a STD of: ' + str(gstd))
+print()
+print('Average g Tim is: ' + str(gt_avg))
+print('With a STD of: ' + str(gt_std))
+print()
+print('Average g Mario is: ' + str(gmar_avg))
+print('With a STD of: ' + str(gmar_std))
+##### PLOT DATA
+
+params = np.polyfit(T, l, 2)
+p = lambda x, a, b, c: a*x**2 + b*x + c
+
+# plt.figure()
+# plt.plot(T, l, 'bo', label='Measurements')
+# plt.plot(T, p(T, *params), 'k--', label='Polyfit')
+# plt.title('Period plotted against length of Pendulum')
+# plt.ylabel('Length in cm')
+# plt.xlabel('Period in s')
+# plt.legend()
+# plt.grid()
+# plt.show()
+
+
+
 
 
